4b7c5ed3556bb364e3ddcc7a0c88acf6443adb96
Tremi Dkhar <tremi@motiveinside.com>
2020-01-11 15:22:15 +0530

    Fix where class of previous icon is embeded with the next icon class

--- class-tsd-get-icon.php
+++ class-tsd-get-icon.php
@@ -147,9 +147,9 @@ if ( ! class_exists( 'TSD_Get_Icons ' ) ) {
 			$this->icon = file_get_contents( $this->icon_path ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents -- The file is a local file
 
 			if ( ! empty( $this->icon_atts['class'] ) ) {
-				$this->class .= ' ' . esc_attr( $this->icon_atts['class'] . ' ' . esc_attr( $this->icon_atts['icon'] ) );
+				$this->class = 'svg-icon ' . esc_attr( $this->icon_atts['class'] . ' ' . esc_attr( $this->icon_atts['icon'] ) );
 			} else {
-				$this->class .= ' ' . esc_attr( $this->icon_atts['icon'] );
+				$this->class = 'svg-icon ' . esc_attr( $this->icon_atts['icon'] );
 			}
 
 			if ( false !== $this->icon_atts['size'] ) {
